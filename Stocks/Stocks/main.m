138ff961b498fa4d275abd4af24dc1ae4f44a12a
Raymond T. Hightower <rth@wisdomgroup.com>
2013-01-28 14:03:54 -0600

    replaced the old Stocks/ app

---
+++ Stocks/Stocks/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Stocks
+//
+//  Created by RTH on 1/28/13.
+//  Copyright (c) 2013 WisdomGroup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
