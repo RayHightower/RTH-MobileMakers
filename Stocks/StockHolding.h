a0f9e2e69c6256f5b2db62ed4b9b06b02279ede9
Raymond T. Hightower <rth@wisdomgroup.com>
2013-01-28 15:45:52 -0600

    beginning next round

---
+++ Stocks/StockHolding.h
@@ -0,0 +1,16 @@
+//
+//  StockHolding.h
+//  Stocks
+//
+//  Created by RTH on 1/28/13.
+//  Copyright (c) 2013 WisdomGroup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StockHolding : NSObject
+
+@property float purchaseSharePrice;
+@property float currentSharePrice;
+
+@end
---
+++ Stocks/StockHolding.m
@@ -0,0 +1,17 @@
+//
+//  StockHolding.m
+//  Stocks
+//
+//  Created by RTH on 1/28/13.
+//  Copyright (c) 2013 WisdomGroup. All rights reserved.
+//
+
+#import "StockHolding.h"
+
+@implementation StockHolding
+
+@synthesize purchaseSharePrice;
+@synthesize currentSharePrice;
+
+
+@end
